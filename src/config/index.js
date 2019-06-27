e603288f24eb951f0c233cf61e30c5447f5701e5
fangyouyu <fangyouyu@bakejinfu.com>
2019-06-27 15:00:01 +0800

    添加线上环境

--- src/config/index.js
+++ src/config/index.js
@@ -2,5 +2,9 @@ export  default {
     DEV:{
         url:'http://127.0.0.1:3333',
         imgUrl:'http://127.0.0.1:3825/'
+    },
+    PRO:{
+        url:'http://47.101.49.14:3333',
+        imgUrl:'http://47.101.49.14:3306/'
     }
 }
